246a52fc3da5cf39f6de4cdcfc6de3002d08226a
hirangayathra6757-dot <hirangayathra.6757@gmail.com>
2025-12-11 20:45:25 +0530

    Add files via upload

---
+++ firebase.js
@@ -0,0 +1,11 @@
+// Firebase config
+const firebaseConfig = {
+  apiKey: "AIzaSyAVUIIwFXEsOmOS96lifn_aEccV3I35bWU",
+  authDomain: "dream-donators.firebaseapp.com",
+  projectId: "dream-donators",
+  storageBucket: "dream-donators.firebasestorage.app",
+  messagingSenderId: "943510265596",
+  appId: "1:943510265596:web:25e49411973c046bf50179",
+  measurementId: "G-KJ1WWR3TJH"
+};
+firebase.initializeApp(firebaseConfig);
